0eb03cba93e1421e7dcf4074559d0c7d503d64ab
zammatommy <71379782+zammatommy@users.noreply.github.com>
2021-03-31 10:13:15 +0200

    Aggiungere i file di progetto.

---
+++ Verifica-31-marzo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verifica_31_marzo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
